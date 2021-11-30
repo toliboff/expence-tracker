dc47b2362efbfe0c245276a7d9a903ccadd24c0e
Tolibjon <tolibjohn@gmail.com>
2021-11-30 16:34:00 +0500

    Create splash screen

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   devise_for :views
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-   root to: 'groups#index'
+   root to: 'splash_screen#index'
    
+   resources :splash_screen, only: %i[index]
    resources :users, only: %i[index show] do 
     resources :groups do
        resources :entities
