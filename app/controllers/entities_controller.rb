c78a240522a2888d5e7a75a193417e55063cd737
Tolibjon <tolibjohn@gmail.com>
2021-11-30 20:17:05 +0500

    Create actions for entities

--- app/controllers/entities_controller.rb
+++ app/controllers/entities_controller.rb
@@ -1,4 +1,31 @@
 class EntitiesController < ApplicationController
   before_action :authenticate_user!
   load_and_authorize_resource
+
+  def new
+    @entity = Entity.new
+    @user = current_user
+    @groups = Group.where(user: @user)
+  end
+
+  def create
+
+    @entity = Entity.new(entity_params)
+    # @entity = Entity.new(entity_params[:name], entity_params[:amount])
+    @entity.author = current_user
+
+    if @entity.save
+      redirect_to user_groups_path(current_user), notice:'Entity added succesfully'
+    else
+      render :new, notice: "Ooops, something went wrong"
+    end
+
+  end
+
+  private
+
+  def entity_params
+    params.require(:entity).permit(:name, :amount, group_ids:[])
+    
+  end
 end
--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -13,7 +13,9 @@ class GroupsController < ApplicationController
 
   def show
     @user = current_user
-    @groups = Group.order(:created_at).where(user: @user)
+    @group = Group.find(params[:id])
+    @entities = @group.entities.order(created_at: :desc)
+    @total = @group.entities.sum(:amount)
   end
 
   def new
