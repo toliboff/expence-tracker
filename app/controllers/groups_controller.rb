1bbcb091b07a1c27ef71e8fda31e8e17ad6e6315
Tolibjon <tolibjohn@gmail.com>
2021-11-30 17:30:04 +0500

    Create actions for Groups controller

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,7 +1,31 @@
 class GroupsController < ApplicationController
   before_action :authenticate_user!
   load_and_authorize_resource
+
   def index
+    @user = current_user
+    @groups = Group.order(:created_at).where(user: @user)
+  end
+
+  def new
+    @user = current_user
+    @group = Group.new
+  end
+
+  def create
+    @group = Group.new group_params
+    @group.user = current_user
     
+    if @group.save
+      redirect_to user_groups_path(current_user)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def group_params
+      params.require(:group).permit(:name, :icon)
   end
 end
