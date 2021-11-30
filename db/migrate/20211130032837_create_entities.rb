e86cc4cb617a6d6768b729f1e7ef7ca978fc228b
Tolibjon <tolibjohn@gmail.com>
2021-11-30 10:00:59 +0500

    Add associations

--- db/migrate/20211130032837_create_entities.rb
+++ db/migrate/20211130032837_create_entities.rb
@@ -3,8 +3,13 @@ class CreateEntities < ActiveRecord::Migration[6.1]
     create_table :entities do |t|
       t.string :name
       t.decimal :amount
+      t.references :user, null: false, foreign_key: true
 
       t.timestamps
+    end
+
+    change_table :entities do |t|
+      t.rename :user_id, :author_id
     end
   end
 end
